1fb1c6c06c9bf2670160829837cb05bc3fe6debe
jellibeans-ted <ted@jellibean.co>
2023-07-28 23:55:29 +0800

    chore: fixed err

--- web/save-data.js
+++ web/save-data.js
@@ -7,26 +7,28 @@ const s3Bucket = "shopify-export";
 const endpoint = new AWS.Endpoint("g1n2.sj.idrivee2-49.com");
 
 const s3 = new AWS.S3({
-    endpoint: endpoint,
-    credentials: {
-        accessKeyId: aws_access_key_id,
-        secretAccessKey: aws_secret_access_key
-    }
+  endpoint: endpoint,
+  credentials: {
+    accessKeyId: aws_access_key_id,
+    secretAccessKey: aws_secret_access_key,
+  },
 });
 
 export const saveData = async (products, customers, shopName, date, ts) => {
-    const objectData = { products, customers: (customers || {}) };
-    const fileName = `${shopName}/${date}/shopify_export_${date}_${ts}.json`;
+  const objectData = { products, customers: customers || {} };
+  const fileName = `${shopName}/${date}/shopify_export_${date}_${ts}.json`;
 
-    try {
-        const result  = await s3.putObject({
-            Bucket: s3Bucket,
-            Key: fileName,
-            Body: JSON.stringify(objectData),
-        });
+  try {
+    const result = await s3
+      .putObject({
+        Bucket: s3Bucket,
+        Key: fileName,
+        Body: JSON.stringify(objectData),
+      })
+      .promise();
 
-    console.log(result);
-    } catch (error) {
-        console.log(error);
-    }
-}
+      console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
